69b6a1a610b6b0470ceef3d9964cfb6770d61dd7
widojansen <widojansen@gmail.com>
2023-09-01 14:05:38 +0200

    set amsterdam time zone for last poll

--- lib/delayed_job_web/application/app.rb
+++ lib/delayed_job_web/application/app.rb
@@ -204,7 +204,7 @@ class DelayedJobWeb < Sinatra::Base
 
   def poll
     if @polling
-      text = "Last Updated: #{Time.now.strftime("%H:%M:%S")}"
+      text = "Last Updated: #{Time.now.in_time_zone('Amsterdam/Europe').strftime("%H:%M:%S")} CET"
     else
       text = "<a href='#{u(request.path_info + ".poll")}' rel='poll'>Live Poll</a>"
     end
